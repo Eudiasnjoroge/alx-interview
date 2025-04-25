a1d83b0b79efc8c7fe8d76c63be0004e9dc3b995
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2025-04-25 15:33:10 +0300

    method that determines if all the boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0-lockboxes.py
+Determines if all boxes can be opened given keys within boxes.
+"""
+
+def canUnlockAll(boxes):
+    """
+    Checks if all boxes can be unlocked
+    :param boxes: List of lists representing boxes and their keys
+    :return: True if all boxes can be opened, False otherwise
+    """
+    if not boxes or not isinstance(boxes, list):
+        return False
+
+    unlocked = set([0])  # Start with box 0
+    keys = [0]  # Stack for DFS
+
+    while keys:
+        current = keys.pop()
+        for key in boxes[current]:
+            if 0 <= key < len(boxes) and key not in unlocked:
+                unlocked.add(key)
+                keys.append(key)
+
+    return len(unlocked) == len(boxes)
